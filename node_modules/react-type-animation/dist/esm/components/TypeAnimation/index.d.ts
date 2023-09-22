aee24d8e5cf796d51837f00e89baf70b8c76c170
Soohyun Choi <soohyun9090@gmail.com>
2023-09-22 01:17:58 -0700

    restructure file system

---
+++ node_modules/react-type-animation/dist/esm/components/TypeAnimation/index.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { TypeAnimationProps } from './index.types';
+declare const _default: React.MemoExoticComponent<React.ForwardRefExoticComponent<Pick<TypeAnimationProps & React.HTMLAttributes<HTMLParagraphElement | HTMLElement | HTMLDivElement | HTMLSpanElement | HTMLAnchorElement | HTMLHeadingElement>, "sequence" | "repeat" | "speed" | "deletionSpeed" | "omitDeletionAnimation" | "preRenderFirstString" | "wrapper" | "cursor" | keyof React.HTMLAttributes<HTMLParagraphElement | HTMLElement | HTMLDivElement | HTMLSpanElement | HTMLAnchorElement | HTMLHeadingElement>> & React.RefAttributes<HTMLParagraphElement | HTMLElement | HTMLDivElement | HTMLSpanElement | HTMLAnchorElement | HTMLHeadingElement>>>;
+export default _default;
