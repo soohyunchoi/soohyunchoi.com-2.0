1c83805384386291c5db7becf95f2e2aadb44c70
Soohyun Choi <soohyun9090@gmail.com>
2023-09-22 02:12:23 -0700

    deploy on github pages for first time

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,7 +8,7 @@ export function Logo(props: LogoProps) {
     return (
         <div className=''>
             <a href='#title'>
-                <img className='object-contain h-full z-30' src='/nextjs-github-pages/assets/images/logo.png' alt='logo'/>
+                <img className='object-contain h-full z-30' src='/assets/images/logo.png' alt='logo'/>
             </a>
         </div>
     );
