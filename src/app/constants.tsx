7aa0658c33730e880bccdcda1d7c0ed93dbe42b0
soohyunchoi <hello@soohyunchoi.com>
2025-07-29 23:49:42 -0700

    update resume link

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -79,7 +79,7 @@ export const BIO_SECTION_CONTENT: BioCardProps[] = [
         secondaryColor: '#C08261',
     },
 ]
-export const RESUME_LINK: string = 'https://drive.google.com/file/d/1YTWzDu--k0YjHd-BOwxaT5ajiNTm_2aK/view?usp=sharing';
+export const RESUME_LINK: string = 'https://drive.google.com/file/d/1bOvOgtbNfKc1F7BBlcaWZWIXnqfP2sG6/view?usp=sharing';
 export const LINKS: any= {
     linkedIn: "https://www.linkedin.com/in/soohyunchoi/",
     gitHub: 'https://github.com/soohyunchoi',
