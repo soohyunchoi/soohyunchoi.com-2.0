247fdfc87a70348d8bc7e8d677b2f6744b54923a
Soohyun Choi <soohyun9090@gmail.com>
2026-03-31 20:37:45 -0700

    Update RESUME_LINK to new Google Drive URL

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -79,7 +79,7 @@ export const BIO_SECTION_CONTENT: BioCardProps[] = [
         secondaryColor: '#C08261',
     },
 ]
-export const RESUME_LINK: string = 'https://drive.google.com/file/d/1IALwdPAkRsscmEJPRYCQw-IFwZV48NNS/view?usp=sharing';
+export const RESUME_LINK: string = 'https://drive.google.com/file/d/1dwCIBo9f2swiSd8T905KPb60orNUqive/view?usp=sharing';
 export const LINKS: any= {
     linkedIn: "https://www.linkedin.com/in/soohyunchoi/",
     gitHub: 'https://github.com/soohyunchoi',
